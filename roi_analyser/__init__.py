2c5641e7be047b0a9a4aef97a669ed59605df6f4
Jakub Povinec <jakub.povinec@gmail.com>
2025-06-30 12:44:16 +0200

    version bump

--- roi_analyser/__init__.py
+++ roi_analyser/__init__.py
@@ -4,5 +4,5 @@ ROI Analyser - A library for microscopy image analysis and ROI detection.
 
 from .processing import RoiAnalyser
 
-__version__ = "0.1.0"
+__version__ = "0.2.1"
 __all__ = ["RoiAnalyser"]
